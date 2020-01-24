7cc6c835abfc4d3982a6a3487a9d899694736589
scvgoodtogo <kowal.a1234@gmail.com>
2020-01-24 23:42:58 +0100

    number input tweaks

--- src/components/generic/naturalNumberInput.tsx
+++ src/components/generic/naturalNumberInput.tsx
@@ -11,14 +11,18 @@ const NaturalNumberInput: React.FC<IProps> = (props) => {
 
 	return <label
 		className={
-			props.className ? props.className : "input-nat-number"
+			"input-nat-number " +
+			props.className
 		}
 	>
 		{props.label}
 		<input
 			type="number"
+			pattern="[0-9]*"
+			inputMode="numeric"
+			formNoValidate
 			onFocus={e => { e.target.select() }}
-			value={props.value} //cleans up visually
+			value={props.value || ' '}
 			onChange={e => {
 				props.callback(validate(props.value, e.target.value))
 			}}
@@ -29,15 +33,15 @@ const NaturalNumberInput: React.FC<IProps> = (props) => {
 export default NaturalNumberInput;
 
 const validate = (currentValue: number, input: string) => {
-	if (input === '') input = '0'; //allows zeroing by deleting input
+	if (input === '') input = '0';//allows zeroing by deleting input
 	const parsedInput = parseNaturalNumber(input);
 	if (isNaN(parsedInput)) {
 		return currentValue;
 	} else return parsedInput;
 };
 
-const parseNaturalNumber = (value: string): number => {
-	const int = parseInt(value);
+const parseNaturalNumber = (value: string, radix: number = 10): number => {
+	const int = parseInt(value, radix);
 
 	if (int >= 0) {
 		return int
